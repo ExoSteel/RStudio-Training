3506005b4003066497aa02839875f93e11d51fb7
Silver <83893730+ExoSteel@users.noreply.github.com>
2024-08-13 20:44:13 +0800

    second

---
+++ 1. Data Types and Variables.R
@@ -0,0 +1,43 @@
+"Hello World!"
+
+1
+2
+3
+
+print(4)   # Does the same thing as above
+
+5 + 5
+
+# This is a COMMENT, it's very useful to mark certain segments of code
+# for increased readability and convenience
+
+
+# integer
+x <- 1000L
+class(x)   # Outputs the data type of the variable
+
+# numeric
+x <- 10.5
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R"
+class(x)
+
+# Variables 
+
+name <- 'Jeff'
+age <- 10
+
+name
+age
+
+new_age <- age + age
+
+new_age
+
+name + age # This will result in an error
---
+++ 2. Maths and Logic.R
@@ -0,0 +1,28 @@
+# Addition
+
+
+
+# Subtraction
+
+
+
+# Multiplication
+
+
+
+# Division
+
+
+
+# Helpful Mathematical Functions
+
+max(5, 10, 15)         # Minimum
+min(5, 10, 15)         # Maximum
+
+sqrt(16)               # Square Root
+
+abs(-4.8)              # Modulus
+
+ceiling(4.3)           # Round Up
+
+floor(4.6)             # Round Down
